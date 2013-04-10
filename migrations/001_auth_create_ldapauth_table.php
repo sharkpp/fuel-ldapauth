af4ca5a882c8c80c7a06c25a75ae262f3348038a
sharkpp <webmaster@sharkpp.net>
2013-04-10 13:54:27 +0000

    マイグレーション実行時に構文エラーとなっていたのを修正
    git-svn-id: http://nasserver:888/repos/main/platform/fuelphp/fuel-ldapauth@2431 fd21481f-4f5f-b64a-a7cc-914020ce4b36

--- migrations/001_auth_create_ldapauth_table.php
+++ migrations/001_auth_create_ldapauth_table.php
@@ -14,15 +14,30 @@ namespace Fuel\Migrations;
 
 class Auth_Create_Ldapauth_Table
 {
-	static private $field_names = array();
+	private $table_name  = '';
+	private $field_names = array();
 
 	/**
 	 * Class initialization
 	 */
 	public static function _init()
 	{
+	}
+
+	/**
+	 * Class construction
+	 */
+	function __construct()
+	{
 		// load the migrations config
 		\Config::load('ldapauth', true);
+
+		$this->table_name =
+			\Config::get('ldapauth.db.table_name',
+			//	\Config::get('simpleauth.table_name', 
+						'users'
+			//		)
+				);
 
 		foreach (array(
 					'username_field',
@@ -33,8 +48,8 @@ class Auth_Create_Ldapauth_Table
 					'profile_fields',
 				) as $field_name)
 		{
-			self::$field_names[$field_name] =
-				\Config::get('ldapauth.'.$field_name,
+			$this->field_names[$field_name] =
+				\Config::get('ldapauth.db.'.$field_name,
 				//	\Config::get('simpleauth.'.$field_name, 
 							preg_replace('/_field$/', '', $field_name)
 				//		)
@@ -45,18 +60,19 @@ class Auth_Create_Ldapauth_Table
 	function up()
 	{
 		\DBUtil::create_table(
-			\Config::get('simpleauth.table_name', 'users'),
+			$this->table_name,
 			array(
-				'id'                                 => array('constraint' => 11,  'type' => 'int', 'auto_increment' => true),
-				self::$field_names['username_field'] => array('constraint' => 50,  'type' => 'varchar'),
-			//	self::$field_names['password_field'] => array('constraint' => 255, 'type' => 'varchar'),
-				self::$field_names['group_field']    => array('constraint' => 11,  'type' => 'int', 'default' => 1),
-				self::$field_names['email_field']    => array('constraint' => 255, 'type' => 'varchar'),
-				self::$field_names['last_login']     => array('constraint' => 25,  'type' => 'varchar'),
-				self::$field_names['login_hash']     => array('constraint' => 255, 'type' => 'varchar'),
-				self::$field_names['profile_fields'] => array(                     'type' => 'text'),
-				'created_at'                         => array('constraint' => 11,  'type' => 'int', 'default' => 0),
-				'updated_at'                         => array('constraint' => 11,  'type' => 'int', 'default' => 0),
+				'id'                                   => array('constraint' => 11,  'type' => 'int', 'auto_increment' => true),
+				$this->field_names['username_field']   => array('constraint' => 50,  'type' => 'varchar'),
+			//	$this->field_names['password_field']   => array('constraint' => 255, 'type' => 'varchar'),
+				$this->field_names['group_field']      => array('constraint' => 11,  'type' => 'int', 'default' => 1),
+				$this->field_names['email_field']      => array('constraint' => 255, 'type' => 'varchar'),
+				$this->field_names['last_login_field'] => array('constraint' => 25,  'type' => 'varchar'),
+				$this->field_names['login_hash_field'] => array('constraint' => 255, 'type' => 'varchar'),
+				$this->field_names['profile_fields']   => array(                     'type' => 'text'),
+				'created_at'                           => array('constraint' => 11,  'type' => 'int', 'default' => 0),
+				'updated_at'                           => array('constraint' => 11,  'type' => 'int', 'default' => 0),
+		), array('id'));
 
 		// add a unique index on username and email
 	//	\DBUtil::create_index('users', array('username', 'email'), 'username', 'UNIQUE');
@@ -64,6 +80,6 @@ class Auth_Create_Ldapauth_Table
 
 	function down()
 	{
-		\DBUtil::drop_table(\Config::get('simpleauth.table_name', 'users'));
+		\DBUtil::drop_table($this->table_name);
 	}
 }
